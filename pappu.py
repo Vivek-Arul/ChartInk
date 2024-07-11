e3858527a1b1315053cf1703ade3db120b952934
Vivek Ar <100012206+Vivek-Arul@users.noreply.github.com>
2024-07-11 20:09:32 +0530

    Update pappu.py

--- pappu.py
+++ pappu.py
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 Charting_Link = "https://chartink.com/screener/"
 Charting_url = 'https://chartink.com/screener/process'
 
-spreadsheet_name = "pappu"
+spreadsheet_name = "suriya"
 worksheet_letter = "p"
 total_no_of_worksheet = 27
 
